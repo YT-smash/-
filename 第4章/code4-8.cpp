af90436f2a6440e3dbc3b51099dd5312c44e64d0
YT-smash <77914653+YT-smash@users.noreply.github.com>
2021-05-06 21:58:37 +0900

    Create code4-8.cpp

---
+++ 第4章/code4-8.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// fibo(N) の答えをメモ化する配列
+vector<long long> memo;
+
+long long fibo(int N) {
+    // ベースケース
+    if (N == 0) {
+        return 0;
+    }
+    else if (N == 1) {
+        return 1;
+    }
+    
+    // メモをチェック
+    if (memo[N] != -1) return memo[N];
+    
+    // 答えをメモ化しながら再帰呼び出し
+    return memo[N] = fibo(N - 1) + fibo(N - 2);
+}
+
+int main() {
+    // メモ化用配列を -1 で初期化する
+    memo.assign(50, -1);
+    
+    // fibo(49) を呼び出す
+    fibo(49);
+    
+    // memo[0], ..., memo[49] に答えが格納されている
+    for (int N = 2; N < 50; ++N) {
+        cout << memo[N] << endl;
+    }
+}
