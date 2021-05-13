45c4baa633ea35e9f95d5257f10a81be009fd666
ytsmash <77914653+ytsmash@users.noreply.github.com>
2021-05-13 20:07:28 +0900

    Create code6-2.cpp

---
+++ 第6章/code6-2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// xが条件を満たすかどうか
+bool P(int x) {
+    
+}
+
+// P(x) == trueとなる最小の整数xを返す
+int binary_search() {
+    int left, right; // P(left) == false, P(right) == trueとなるように
+    
+    while (right - left > 1) {
+        int mid = left + (right - left) / 2;
+        if (P(mid)) right = mid;
+        else left = mid;
+    }
+    return right;
+}
